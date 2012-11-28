5bf0ca41b41cb1e8e0e492275c84ab52e309aaff
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-11-28 19:58:26 +0100

    Ändrade design

--- Hemsida/assets/pages/login.php
+++ Hemsida/assets/pages/login.php
@@ -1,11 +1,10 @@
 <?php include('_top.php'); ?>
-<?php include('_meny.php'); ?>
-<form class="form-signin" action="?page=process.php" method="POST">
+<form class="form-signin" action="?page=process&do=login" method="POST">
 	<h2 class="form-signin-heading">Var vänlig att logga in</h2>
-	<input name="username" type="text" class="input-block-level" placeholder="Email address">
-	<input name="password" type="password" class="input-block-level" placeholder="Password">
+	<input name="username" type="text" class="input-block-level" placeholder="Email">
+	<input name="password" type="password" class="input-block-level" placeholder="Lösenord">
 	<button class="btn btn-large btn-success btn-block" type="submit">Logga in</button>
 	<hr>
-	<center>Inte medlem? <a href="#">Registrera dig här</a></center>
+	<center>Inte medlem? <a href="?page=Registrera">Registrera dig här</a></center>
 </form>
 <?php include('_bottom.php'); ?>
