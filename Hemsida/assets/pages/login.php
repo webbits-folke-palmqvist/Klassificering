f9b59695a8cbabcb5a9136696911e49803638f19
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-11-28 16:21:59 +0100

    Ändrade vart man vart skickad och vilken metod man använde

--- Hemsida/assets/pages/login.php
+++ Hemsida/assets/pages/login.php
@@ -1,6 +1,6 @@
 <?php include('_top.php'); ?>
 <?php include('_meny.php'); ?>
-<form class="form-signin">
+<form class="form-signin" action="?page=process.php" method="POST">
 	<h2 class="form-signin-heading">Var vänlig att logga in</h2>
 	<input name="username" type="text" class="input-block-level" placeholder="Email address">
 	<input name="password" type="password" class="input-block-level" placeholder="Password">
