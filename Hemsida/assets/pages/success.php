df8b441dab8a444194e3da7a6115a168e95f58a7
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-11-29 11:02:27 +0100

    En lyckad sida

---
+++ Hemsida/assets/pages/success.php
@@ -0,0 +1,9 @@
+<?php include('_top.php'); ?>
+<form class="form-signin">
+	<center>
+		<h3><?php success(); ?></h3>
+		<p>Du kan nu <a href="?page=Logga-in">logga in</a>.</p>
+	</center>
+	<?php if(!$_SESSION['success']){header('location: ?page=Hem');} ?>
+</form>
+<?php include('_bottom.php'); ?>
