d5a9963b99a3e52ed32ee846a632ed766fc7c65a
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-11-29 11:02:58 +0100

    Ändrade så man använder användarnamn istället för mail

--- Hemsida/assets/pages/login.php
+++ Hemsida/assets/pages/login.php
@@ -2,7 +2,7 @@
 <form class="form-signin" action="?page=Process&action=login" method="POST">
 	<h2 class="form-signin-heading">Var vänlig att logga in</h2>
 	<h5><?php error(); ?></h5>
-	<input name="username" type="text" class="input-block-level" placeholder="Email">
+	<input name="username" type="text" class="input-block-level" placeholder="Användarnamn">
 	<input name="password" type="password" class="input-block-level" placeholder="Lösenord">
 	<button class="btn btn-large btn-success btn-block" type="submit">Logga in</button>
 	<hr>
