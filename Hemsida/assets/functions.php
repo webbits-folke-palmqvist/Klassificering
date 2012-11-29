85c40a18747b5ada2e0a21bd9733c30f7d504c3b
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-11-29 11:02:38 +0100

    La till nya funktioner

--- Hemsida/assets/functions.php
+++ Hemsida/assets/functions.php
@@ -14,6 +14,19 @@ function unset_error() {
 	unset($_SESSION['error']);
 }
 
+function success() {
+	echo '<p class="text-success">'.$_SESSION['success'].'</p>';
+	unset_error();
+}
+
+function set_success($success) {
+	$_SESSION['success'] = $success;
+}
+
+function unset_success() {
+	unset($_SESSION['success']);
+}
+
 function login(){
 	if(!$_SESSION['user']) {
 		header('location: ?page=Logga-in');
