ee45ebc05dcd058dd95b25508fa9c425aae9b5c3
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-09 21:08:57 +0100

    Skriver ut alla användarens kategorier

--- Hemsida/assets/pages/home.php
+++ Hemsida/assets/pages/home.php
@@ -1,54 +1,24 @@
 <?php
 include('_top.php');
 login();
+
+$user_id = user_id($_SESSION['user']);
 ?>
 <div class="hero-unit">
 	<ul class="breadcrumb">
 		<li class="active">Start</li>
 		<li class="pull-right"><?php if(rank() == 9){ ?><a class="btn" href="?page=Admin">Admin panel</a><?php } ?> <a class="btn btn-inverse" href="#">Mitt konto</a> <a class="btn btn-danger" href="?page=Process&action=logout">Logga ut</a></li>
 	</ul>
-	<a class="btn btn-success" href="?page=Dokument">Lägg till ett dokument</a> <a class="btn btn-success" href="#">Lägg till en kategori</a>
+	<a class="btn btn-success" href="#">Lägg till en kategori</a>
 	<br /><br />
-	<div class="accordion" id="accordion2">
-		<div class="accordion-group">
-			<div class="accordion-heading">
-				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#1">
-					Formal letter
-				</a>
-			</div>
-			<div id="1" class="accordion-body collapse" style="height: 0px;">
-				<div class="accordion-inner">
-					Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. 
-					<hr><a class="btn btn-info" href="#">Ändra</a> <a class="btn btn-danger" href="#">Radera</a>
-				</div>
-			</div>
-		</div>
-		<div class="accordion-group">
-			<div class="accordion-heading">
-				<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#2">
-					Company assignment
-				</a>
-			</div>
-			<div id="2" class="accordion-body collapse" style="height: 0px;">
-				<div class="accordion-inner">
-					Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-					<hr><a class="btn btn-info" href="#">Ändra</a> <a class="btn btn-danger" href="#">Radera</a>
-				</div>
-			</div>
-		</div>
-		<div class="accordion-group">
-			<div class="accordion-heading">
-				<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#3">
-					Hotdogs
-				</a>
-			</div>
-			<div id="3" class="accordion-body collapse" style="height: 0px;">
-				<div class="accordion-inner">
-					Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-					<hr><a class="btn btn-info" href="#">Ändra</a> <a class="btn btn-danger" href="#">Radera</a>
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php
+	$result = mysql_query("SELECT * FROM category WHERE deleted = '0' AND user_id = '$user_id'");
+
+	while ($row = mysql_fetch_array($result)) {
+		?>
+		<p><a href="?page=Kategori&?cat_id=<?php echo $row['id']; ?>"><?php echo $row['name']; ?></a></p>
+		<?php
+	}
+	?>
 </div>
 <?php include('_bottom.php'); ?>
