95c5ff0bc31b5d4758daad19edd163322652e6b4
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-17 13:41:26 +0100

    Fixade felaktiga länkar

--- Hemsida/assets/pages/category.php
+++ Hemsida/assets/pages/category.php
@@ -17,7 +17,7 @@ if($_GET['action'] == "view"){
 			<li class="pull-right"><?php if(rank() == 9){ ?><a class="btn" href="?page=Admin">Admin panel</a><?php } ?> <a class="btn btn-inverse" href="#">Mitt konto</a> <a class="btn btn-danger" href="?page=Process&action=logout">Logga ut</a></li>
 		</ul>
 		<a class="btn btn-success" href="?page=Dokument&action=add&cat_id=<?php echo $cat_id; ?>">Lägg till ett dokument</a>
-		<br /><br />
+		<?php success(); ?>
 		<div class="accordion" id="accordion2">
 			<div class="accordion-group">
 				<?php
@@ -26,11 +26,11 @@ if($_GET['action'] == "view"){
 				while($row = mysql_fetch_array($result)) {
 					?>
 					<div class="accordion-heading">
-						<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#1">
+						<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#<?php echo $row['id']; ?>">
 							<?php echo $row['title']; ?>
 						</a>
 					</div>
-					<div id="1" class="accordion-body collapse" style="height: 0px;">
+					<div id="<?php echo $row['id']; ?>" class="accordion-body collapse" style="height: 0px;">
 						<div class="accordion-inner">
 							<?php echo $row['content']; ?>
 							<hr><a class="btn btn-info" href="#">Ändra</a> <a class="btn btn-danger" href="#">Radera</a>
