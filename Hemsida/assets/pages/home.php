46d1f7dc8a6659a3a5fb9e418818d985ceb2158c
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-17 15:26:45 +0100

    Ändrade menyn

--- Hemsida/assets/pages/home.php
+++ Hemsida/assets/pages/home.php
@@ -7,7 +7,7 @@ $user_id = user_id($_SESSION['user']);
 <div class="hero-unit">
 	<ul class="breadcrumb">
 		<li class="active">Start</li>
-		<li class="pull-right"><?php if(rank() == 9){ ?><a class="btn" href="?page=Admin">Admin panel</a><?php } ?> <a class="btn btn-inverse" href="#">Mitt konto</a> <a class="btn btn-danger" href="?page=Process&action=logout">Logga ut</a></li>
+		<li class="pull-right"><?php if(rank() == 9){ ?><a class="btn" href="?page=Admin">Admin panel</a><?php } ?> <a class="btn btn-inverse" href="?page=Mitt-konto">Mitt konto</a> <a class="btn btn-danger" href="?page=Process&action=logout">Logga ut</a></li>
 	</ul>
 	<a class="btn btn-success" href="?page=Kategori&action=add">Lägg till en kategori</a>
 	<?php success(); ?>
