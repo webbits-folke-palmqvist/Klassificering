d71c66a774601e675ed0e6fe4c75003628278768
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-17 15:51:58 +0100

    Ändrade utseende

--- Hemsida/assets/pages/admin_users.php
+++ Hemsida/assets/pages/admin_users.php
@@ -1,23 +1,23 @@
 <table class="table table-bordered">
-<tr>
-	<td><strong>ID</strong></td>
-	<td><strong>Användarnamn</strong></td>
-	<td><strong>Ändra</strong></td>
-	<td><strong>Ta bort</strong></td>
-</tr>
-<?php
-$query = "SELECT * FROM users WHERE rank = '1' ORDER BY username"; 
-$result = mysql_query($query) or die(mysql_error());
-
-while($row = mysql_fetch_array($result)){
-	?>
 	<tr>
-		<td><?php echo $row['id']; ?></td>
-		<td><?php echo $row['username']; ?></td>
-		<td><a class="btn" href="#">Ändra</a></td>
-		<td><a class="btn" href="#">Ta bort</a></td>
+		<td><strong>ID</strong></td>
+		<td><strong>Användarnamn</strong></td>
+		<td><strong>Ändra</strong></td>
+		<td><strong>Ta bort</strong></td>
 	</tr>
 	<?php
-}
-?>
+	$query = "SELECT * FROM users WHERE rank = '1' ORDER BY username"; 
+	$result = mysql_query($query) or die(mysql_error());
+
+	while($row = mysql_fetch_array($result)){
+		?>
+		<tr>
+			<td><?php echo $row['id']; ?></td>
+			<td><?php echo $row['username']; ?></td>
+			<td><a class="btn" href="#">Ändra</a></td>
+			<td><a class="btn" href="#">Ta bort</a></td>
+		</tr>
+		<?php
+	}
+	?>
 </table>
