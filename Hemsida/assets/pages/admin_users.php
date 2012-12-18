7ace67f431de6a777da4e4d1d1ad94f2cd8adc64
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-18 13:06:11 +0100

    Ändrade upplägget och översynen på adminpanelen

--- Hemsida/assets/pages/admin_users.php
+++ Hemsida/assets/pages/admin_users.php
@@ -2,7 +2,8 @@
 	<tr>
 		<td><strong>ID</strong></td>
 		<td><strong>Användarnamn</strong></td>
-		<td><strong>Ändra</strong></td>
+		<td><strong>Dokument</strong></td>
+		<td><strong>Kategori</strong></td>
 		<td><strong>Ta bort</strong></td>
 	</tr>
 	<?php
@@ -10,12 +11,14 @@
 	$result = mysql_query($query) or die(mysql_error());
 
 	while($row = mysql_fetch_array($result)){
+		$user_id = user_id($row['username']);
 		?>
 		<tr>
-			<td><?php echo $row['id']; ?></td>
+			<td style="width: 1%;"><?php echo $row['id']; ?></td>
 			<td><?php echo $row['username']; ?></td>
-			<td><a class="btn" href="#">Ändra</a></td>
-			<td><a class="btn" href="#">Ta bort</a></td>
+			<td style="width: 10%;"><?php count_rows_user("document", $user_id); ?> st</td>
+			<td style="width: 10%;"><?php count_rows_user("category", $user_id); ?> st</td>
+			<td style="width: 10%;"><a class="btn" href="#">Ta bort</a></td>
 		</tr>
 		<?php
 	}
