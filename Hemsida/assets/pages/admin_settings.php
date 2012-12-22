7206fcb8160fac5b6d48f35ea16e293069b57be9
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-22 14:34:23 +0100

    La till sidan "Inställningar"

---
+++ Hemsida/assets/pages/admin_settings.php
@@ -0,0 +1,21 @@
+<center><p><strong>Registrering</strong></p></center>
+<table class="table table-bordered">
+	<tr>
+		<td>Registrering</td>
+		<td style="width:250px;">
+			<div class="btn-group">
+				<a class="btn disabled" href="#">På</a>
+				<a class="btn" href="#">Av</a>
+			</div>
+		</td>
+	</tr>
+	<tr>
+		<td>Kräv email</td>
+		<td style="width:250px;">
+			<div class="btn-group">
+				<a class="btn" href="#">På</a>
+				<a class="btn disabled" href="#">Av</a>
+			</div>
+		</td>
+	</tr>
+</table>
