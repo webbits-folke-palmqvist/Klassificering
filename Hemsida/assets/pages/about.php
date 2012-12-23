60f28692e105b3f4cf33d75252208648f99abe3d
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-23 18:08:58 +0100

    Fixat utseendet

--- Hemsida/assets/pages/about.php
+++ Hemsida/assets/pages/about.php
@@ -1,6 +1,10 @@
 <?php include('_top.php'); ?>
-<?php include('_meny.php'); ?>
 <div class="hero-unit">
+	<ul class="breadcrumb">
+		<li><a class="btn" href="?page=Hem">Hem</a></li>
+		<li><a class="btn" href="?page=Om-oss">Om oss</a></li>
+		<li class="pull-right"><a class="btn" href="?page=Logga-in">Logga in</a></li>
+	</ul>
 	<p>Om oss</p>
 </div>
 <?php include('_footer.php'); ?>
--- Hemsida/assets/pages/home.php
+++ Hemsida/assets/pages/home.php
@@ -2,8 +2,9 @@
 <div class="hero-unit">
 	<ul class="breadcrumb">
 		<li><a class="btn" href="?page=Hem">Hem</a></li>
+		<li><a class="btn" href="?page=Om-oss">Om oss</a></li>
 		<li class="pull-right"><a class="btn" href="?page=Logga-in">Logga in</a></li>
 	</ul>
-	<p>asd</p>
+	<p>Hem</p>
 </div>
 <?php include('_footer.php'); ?>
