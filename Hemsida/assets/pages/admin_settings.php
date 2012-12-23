f81ac28fc97f6d3fbb0fb2a295ce5341e63ddeec
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-23 18:54:55 +0100

    Fixade admin inställnings sidan

--- Hemsida/assets/pages/admin_settings.php
+++ Hemsida/assets/pages/admin_settings.php
@@ -1,34 +1,65 @@
 <center><h2>Denna sida är ej fungerande än.</h2></center>
-<center><p><strong>Sidan</strong></p></center>
+<center><p><strong>Allmänt</strong></p></center>
 <table class="table table-bordered">
-	<tr>
-		<td>Nere för underhåll</td>
-		<td style="width:250px;">
-			<div class="btn-group">
-				<a class="btn" href="#">På</a>
-				<a class="btn disabled">Av</a>
-			</div>
-		</td>
-	</tr>
+	<?php
+	$query = "SELECT * FROM settings WHERE setting = 'Allmänt' ORDER BY name"; 
+	$result = mysql_query($query) or die(mysql_error());
+
+	while($row = mysql_fetch_array($result)){
+		?>
+		<tr>
+			<td><?php echo $row['name']; ?></td>
+			<td style="width:250px;">
+				<div class="btn-group">
+					<?php
+					if($row['onoroff'] == 0){ 
+						?>
+						<a class="btn" href="#">På</a>
+						<a class="btn disabled">Av</a>
+						<?php
+					} elseif ($row['onoroff'] == 1) {
+						?>
+						<a class="btn disabled">På</a>
+						<a class="btn" href="#">Av</a>
+						<?php
+					}
+					?>
+				</div>
+			</td>
+		</tr>
+		<?php
+	}
+	?>
 </table>
 <center><p><strong>Registrering</strong></p></center>
 <table class="table table-bordered">
-	<tr>
-		<td>Registrering</td>
-		<td style="width:250px;">
-			<div class="btn-group">
-				<a class="btn disabled">På</a>
-				<a class="btn" href="#">Av</a>
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td>Kräv email</td>
-		<td style="width:250px;">
-			<div class="btn-group">
-				<a class="btn" href="#">På</a>
-				<a class="btn disabled">Av</a>
-			</div>
-		</td>
-	</tr>
+	<?php
+	$query = "SELECT * FROM settings WHERE setting = 'Registrering' ORDER BY name"; 
+	$result = mysql_query($query) or die(mysql_error());
+
+	while($row = mysql_fetch_array($result)){
+		?>
+		<tr>
+			<td><?php echo $row['name']; ?></td>
+			<td style="width:250px;">
+				<div class="btn-group">
+					<?php
+					if($row['onoroff'] == 0){ 
+						?>
+						<a class="btn" href="#">På</a>
+						<a class="btn disabled">Av</a>
+						<?php
+					} elseif ($row['onoroff'] == 1) {
+						?>
+						<a class="btn disabled">På</a>
+						<a class="btn" href="#">Av</a>
+						<?php
+					}
+					?>
+				</div>
+			</td>
+		</tr>
+		<?php
+	}
+	?>
 </table>
