d76a544d532010dd73a3abd394c7c8744e86a87f
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-23 18:04:48 +0100

    La till facebook like knapp

--- Hemsida/assets/pages/_footer.php
+++ Hemsida/assets/pages/_footer.php
@@ -1,6 +1,14 @@
 		</div>
 		<div class="footer">
-			<a class="btn" target="_Blank" href="http://webbits.nu/">klassificering.se, ett påhitt av Webbits</a>
+			<div class="fb-like" data-href="https://www.facebook.com/pages/klassificeringse/314430371989729" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-colorscheme="light"></div>
+			<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = "//connect.facebook.net/sv_SE/all.js#xfbml=1&appId=210380352361672";
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));</script>
 		</div>
 	<?php require('assets/js/stats.js'); ?>
 	</body>
