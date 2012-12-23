e456ead77318cbf5501c2f709abc8189a4be9110
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-23 18:05:15 +0100

    La till extra knappar i menyn

--- Hemsida/assets/pages/home.php
+++ Hemsida/assets/pages/home.php
@@ -1,7 +1,8 @@
 <?php include('_top.php'); ?>
 <div class="hero-unit">
 	<ul class="breadcrumb">
-		<li><a class="btn" href="#">Test</a></li>
+		<li><a class="btn" href="?page=Hem">Hem</a></li>
+		<li class="pull-right"><a class="btn" href="?page=Logga-in">Logga in</a></li>
 	</ul>
 	<p>asd</p>
 </div>
